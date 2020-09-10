52af7f0fbf7515e8a6b81c097aad97e463ac9c25
cseibert2667 <cseibert2667@gmail.com>
2020-09-10 15:08:15 -0700

    orm.js keys/functions defined

--- config/orm.js
+++ config/orm.js
@@ -2,23 +2,35 @@ const connection = require("./connection.js");
 const e = require("express");
 
 const orm = {
-    selectAll: function(table, col, val) {
-        const query = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(query, [table, col, val], function(err,data){
+  selectAll: function (table, col, val, cb) {
+    const query = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(query, [table, col, val], function (err, res) {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+  insertOne: function (table, nameCol, devouredCol, nameVal, devouredVal, cb) {
+    const query = "INSERT INTO ?? (??,??) VALUES (?,?)";
+    connection.query(
+      query,
+      [table, nameCol, devouredCol, nameVal, devouredVal],
+      function (err, res) {
+        if (err) throw err;
+        cb(res);
+      }
+    );
+  },
+  updateOne: function (table, devouredCol, devouredVal, nameCol, nameVal, cb) {
+    const query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+    connection.query(
+        query,
+        [table, devouredCol, devouredVal, nameCol, nameVal],
+        function (err, res) {
             if (err) throw err;
-            console.log(data);
-        })
-    },
-    insertOne: function(table, nameCol, devouredCol, nameVal, devouredVal) {
-        const query = "INSERT INTO ?? (??,??) VALUES (?,?)";
-        connection.query(query, [table, nameCol, devouredCol, nameVal, devouredVal], function(err,data){
-            if (err) throw err;
-            console.log(data)
-        })
-    },
-    updateOne: function() {
-        
-    }
-}
+            cb(res)
+        }
+    )
+  },
+};
 
 module.exports = orm;
