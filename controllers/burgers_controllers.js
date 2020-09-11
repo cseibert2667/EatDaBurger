3baa33e7f111c1c25a0c603c5cba02581663b41c
cseibert2667 <cseibert2667@gmail.com>
2020-09-10 23:44:56 -0700

    ajax calls defined in script.js - app is fully functional (minimal testing)

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -15,13 +15,13 @@ router.get("/", function(req,res){
 });
 
 router.post("/api/burgers", function(req,res){
-    burger.insertOne("burger_name", "devoured", req.body.name, req.body.dev, function(result){
+    burger.insertOne("burger_name", "devoured", req.body.burger_name, req.body.devoured, function(result){
         res.json({ id: result.insertId});
     });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-    burger.updateOne("devoured", req.body.dev, "id", req.body.id, function(result){
+    burger.updateOne("devoured", req.body.devoured, "id", req.params.id, function(result){
         if (result.changedRows == 0) {
             return res.status(404).end();
         } else {
